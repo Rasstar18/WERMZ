d913413a30d5cae8422d28114e6689d3fa927f1f
Rasstar18 <rasmus.florin@elev.ga.ntig.se>
2022-04-08 14:39:11 +0200

    adminsida

---
+++ adminsidan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inlogg</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" id="namn" name="namn" placeholder="Användarnamn">
+        <input type="password" id="losenord" name="losenord" placeholder="Lösenord">
+        <input type="submit" value="Logga in">
+    </form>
+</body>
+</html>
