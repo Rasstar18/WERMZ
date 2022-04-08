2383f0e6dcc8346925d78f5db30fd32ebd9b0485
Rasstar18 <rasmus.florin@elev.ga.ntig.se>
2022-04-08 12:01:17 +0200

    en liten ändring

--- inlogg.php
+++ inlogg.php
@@ -36,9 +36,13 @@ if(!empty($_POST)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inlogg</title>
 </head>
 <body>
-    
+    <form method="POST">
+        <input type="text" id="namn" name="namn" placeholder="Användarnamn">
+        <input type="password" id="losenord" name="losenord" placeholder="Lösenord">
+        <input type="submit" value="Logga in">
+    </form>
 </body>
 </html>
