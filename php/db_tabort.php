129a383ec9ba6f5d74b9c268baed7f41419e98a2
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-04-22 11:31:23 +0200

    Skapade db och db_tabort

---
+++ php/db_tabort.php
@@ -0,0 +1,46 @@
+<?php
+require_once('db.php');
+
+//Hämtar 'type'-värdet (vilken databas som ska ändras)
+if(isset($_GET['type'])) {
+    $type = $_GET['type'];
+}
+else {
+    echo "ERROR: 'type' är inte satt";
+    die();
+}
+
+//Hämtar 'id'-värdet (vilket objekt i databasen som ska tas bort)
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+else {
+    echo "ERROR: 'id' är inte satt";
+    die();
+}
+
+// Bestämmer vilken databas som skall ändras
+if($type == "a") {
+    $table = "anvandare";
+} elseif ($type == "s") {
+    $table = "spel";
+} elseif ($type == "k") {
+    $table = "kategorier";
+} else {
+    echo "ERROR: 'type' har otillåtet värde (får enbart vara 'a', 's', eller 'k')";
+    die();
+}
+
+//DELETE FROM $table WHERE some_column = some_value
+$sql = "DELETE FROM $table WHERE id = $id";
+$result = mysqli_query($conn,$sql);
+
+if ($conn->query($sql) === TRUE) {
+    echo "Objektet togs bort";
+} else {
+    echo "ERROR: Kunde inte ta bort objektet - " . $conn->error;
+}
+  
+$conn->close();
+
+?>
