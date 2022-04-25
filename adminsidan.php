ae1f0dc9d23c35f631f9459abf25959876e008c8
Rasstar18 <rasmus.florin@elev.ga.ntig.se>
2022-04-25 14:18:12 +0200

    changes

--- adminsidan.php
+++ adminsidan.php
@@ -1,3 +1,7 @@
+<?php
+    require_once("inlogg.php");
+    echo $_SESSION['namn'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,23 +9,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="goupg.css">
 </head>
 <body>
     <form method="POST">
-        <input type="text" id="id" value="ID">
-        <input type="text" id="kategori" value="Kategori">
-        <input type="submit" id="verkstall" value="Execute order 67">
-        <?php
-        if(!empty($_POST)){
-            require_once("inlogg.php");
-            $worth = $_POST;
-            $ID = $worth['id'];
-            $category = $worth['kategori'];
-
-            $sql = "SELECT * FROM anvandare WHERE id = $ID"
-        }
-        ?>
     </form>
 </body>
 </html>
