da0b7ff7c9c263b63d123d0c06fec9f097bbc838
spookiebruh <zackeboibeat@gmail.com>
2022-04-25 14:10:16 +0200

    CSS

--- user.php
+++ user.php
@@ -18,48 +18,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="goupg.css">
     <title>User</title>
 </head>
 <body>
-    <?php 
-     //  if ($conn->connect_error) {
-     //   die("Connection failed: " . $conn->connect_error);
-     // }
-     // echo "Connected successfully";
+  <div id="container">
+    <div id="main">
+      <div id="information">
+        <?php 
+        //  if ($conn->connect_error) {
+        //   die("Connection failed: " . $conn->connect_error);
+        // }
+        // echo "Connected successfully";
 
-     $sql = "SELECT * FROM kategorier";
-     $result = $conn->query($sql);
+        $sql = "SELECT * FROM kategorier";
+        $result = $conn->query($sql);
 
-     if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo "Kategori: " . $row["namn"]. "<br>";
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo "Kategori: " . $row["namn"]. "<br>";
+            }
+          } else {
+            echo "0 results";
+          }
+          $conn->close();
+        
+        ?>
+      
+        <?php
+        for ($i=0;$i<1;$i++){
+        ?>
+        <form id="rating">
+          <input type="radio" id="spelbarhet1" name="spelbarhet" value=1>
+          <label for="spelbarhet">1</label><br>
+          <input type="radio" id="spelbarhet2" name="spelbarhet" value=2>
+          <label for="spelbarhet">2</label><br>
+          <input type="radio" id="spelbarhet3" name="spelbarhet" value=3>
+          <label for="spelbarhet">3</label><br>
+          <input type="radio" id="spelbarhet4" name="spelbarhet" value=4>
+          <label for="spelbarhet">4</label><br>
+          <input type="radio" id="spelbarhet5" name="spelbarhet" value=5>
+          <label for="spelbarhet">5</label><br>
+        </form>
+        <?php
         }
-      } else {
-        echo "0 results";
-      }
-      $conn->close();
-    
-    ?>
-    <?php
-    for ($i=0;$i<1;$i++){
-    ?>
-    <form id="spelbarhetrating">
-      <input type="radio" id="spelbarhet1" name="spelbarhet" value=1>
-      <label for="spelbarhet">1</label><br>
-      <input type="radio" id="spelbarhet2" name="spelbarhet" value=2>
-      <label for="spelbarhet">2</label><br>
-      <input type="radio" id="spelbarhet3" name="spelbarhet" value=3>
-      <label for="spelbarhet">3</label><br>
-      <input type="radio" id="spelbarhet4" name="spelbarhet" value=4>
-      <label for="spelbarhet">4</label><br>
-      <input type="radio" id="spelbarhet5" name="spelbarhet" value=5>
-      <label for="spelbarhet">5</label><br>
-    </form>
-    <?php
-    }
-    
-    ?>
+        ?>
+      </div>
+    </div>
+  </div>
     
 
 </body>
